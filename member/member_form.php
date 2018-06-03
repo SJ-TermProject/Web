4d4b71f253a298c3f704b71d06bfd339e579fab7
KwonSuBin <kwonsubin@KwonSuBinui-MacBook-Pro.local>
2018-06-04 00:07:00 +0900

    add signup css

--- member/member_form.php
+++ member/member_form.php
@@ -7,6 +7,14 @@
 <meta charset="utf-8">
 <link href="../css/common.css" rel="stylesheet" type="text/css" media="all">
 <link href="../css/member.css" rel="stylesheet" type="text/css" media="all">
+<style>
+
+td{
+  padding-left: 10px;
+  border: 1px solid #848484;
+}
+
+</style>
 <script>
    function check_id() {
      window.open("check_id.php?id=" + document.member_form.id.value, "IDcheck", "left=200,top=200,width=200,height=60,scrollbars=no,resizable=yes");
@@ -88,6 +96,58 @@
 			<img src="../img/title_join.gif">
 		</div>
 
+    <table id="form_join">
+
+      <tr>
+        <td bgcolor="lightgray">* 아이디(학번)</td>
+        <td><input type="text" name="id"><a href="#">&nbsp;&nbsp;<img src="../img/check_id.gif" onclick="check_id()"></a>&nbsp;&nbsp;&nbsp;8~10자의 숫자만 사용할 수 있습니다.</td>
+      </tr>
+      <tr>
+        <td bgcolor="lightgray">* 비밀번호</td>
+        <td><input type="password" name="pw"></td>
+      </tr>
+      <tr>
+        <td bgcolor="lightgray">* 비밀번호 확인</td>
+        <td><input type="password" name="pw_confirm"></td>
+      </tr>
+      <tr>
+        <td bgcolor="lightgray">* 이름</td>
+        <td><input type="text" name="name"></td>
+      </tr>
+      <tr>
+        <td bgcolor="lightgray">* 휴대폰</td>
+        <td><select class="hp" name="hp1">
+          <option value='010'>010</option>
+          <option value='011'>011</option>
+          <option value='016'>016</option>
+          <option value='017'>017</option>
+          <option value='018'>018</option>
+          <option value='019'>019</option>
+          </select>  - <input type="text" class="hp" name="hp2"> - <input type="text" class="hp" name="hp3"></td>
+      </tr>
+      <tr>
+        <td bgcolor="lightgray">&nbsp;&nbsp;&nbsp;이메일</td>
+        <td><input type="text" id="email1" name="email1"> @ <input type="text" name="email2"></td>
+      </tr>
+      <tr>
+        <td bgcolor="lightgray">&nbsp;&nbsp;&nbsp;생일</td>
+        <td><input type="text" id="birth_day" value="ex.911102"></td>
+      </tr>
+
+        <td id="must">
+          * 는 필수 입력항목입니다.^^
+        </td>
+        <td>
+          <div id="button" style="border-right: none;"><a href="#"><img src="../img/button_save.gif" onclick="check_input()"></a>&nbsp;&nbsp;
+      		                 <a href="#"><img src="../img/button_reset.gif" onclick="reset_form()"></a>
+          </div>
+        </td>
+
+
+    </table>
+
+
+<!--
 		<div id="form_join">
 			<div id="join1">
 			  <ul>
@@ -124,8 +184,8 @@
 			<div id="must"> * 는 필수 입력항목입니다.^^</div>
 		</div>
 
-		<div id="button"><a href="#"><img scr="../img/button_save.gif" onclick="check_input()"></a>&nbsp;&nbsp;
-		                 <a href="#"><img scr="../img/button_reset.gif" onclick="reset_form()"></a></div>
+		<div id="button"><a href="#"><img src="../img/button_save.gif" onclick="check_input()"></a>&nbsp;&nbsp;
+		                 <a href="#"><img src="../img/button_reset.gif" onclick="reset_form()"></a></div> -->
 	    </form>
 	</div> <!-- end of col2 -->
 </div>
