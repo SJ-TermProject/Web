b8e1ce7f265ec48c15257ab5ca0973b44c3a12fd
KwonSuBin <kwonsubin@KwonSuBinui-MacBook-Pro.local>
2018-06-04 18:57:31 +0900

    change link

--- notice/main_menu.php
+++ notice/main_menu.php
@@ -13,6 +13,6 @@
     <a class="nav-link" href="../anonym_board/list.php">익명게시판</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="../survey/survey.php">설문조사</a>
+    <a class="nav-link" href="../survey/survey.php" onClick="window.open(this.href, '', 'width=400, height=300'); return false;">설문조사</a>
   </li>
 </ul>
