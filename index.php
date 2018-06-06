0f8c3165ef33d3e3433a95dd75cfad876b6aaa8c
KwonSuBin <kwonsubin@KwonSuBinui-MacBook-Pro.local>
2018-06-06 20:40:06 +0900

    change ui

--- index.php
+++ index.php
@@ -135,30 +135,15 @@
   </div>
 
 
-<!--배ㄴㅓ -->
-  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="..." alt="첫번째 슬라이드">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="두번째 슬라이드">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="세번째 슬라이드">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">이전</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">다음</span>
-  </a>
-</div>
-    <!--달력 일정-->
+<!--배너 -->
+
+
+
     <div class="container">
+
+
+
+      <!--달력 일정-->
       <h3><a href='?ym=<?php echo $prev; ?>'> &lt; </a><?php echo $html_title; ?><a href="?ym=<?php echo $next; ?>"> &gt; </a></h3>
       <br>
       <table class="table table-borderd">
